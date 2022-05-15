ade3be88e6d6f2d1c64e94be23eb80e29c05e531
aorlanova <nina.orlanova88@gmail.com>
2022-05-15 13:08:10 -0400

    bobo

--- validParenthUp.js
+++ validParenthUp.js
@@ -1,9 +1,12 @@
 let testSubject = "[{}]{[}]";
-let openStoreCurly = [];
-let openStoreRound = [];
-let openStoreSquare = [];
-let closeStoreCurly = [];
-let closeStoreRound = [];
+let openStore = [];
+let closeStore = [];
+let totalOpen = 0;
+let totalClose = 0;
+let validPairs = 0;
+let itterations = 0;
+let validRuns = 0;
+
 let closeStoreSquare = [];
 
 function validParenth(parenth) {
@@ -15,18 +18,80 @@ function validParenth(parenth) {
       console.log("Invalid Parenth");
       break;
 
-    } else {
+    } else if (parenth[i] == "]" || parenth[i] == "}" || parenth[i] == ")") {
 
-      if (parenth[i] == "{") {
+      closeStore.push(i);
+       
+  } else {
 
-	openStoreCurly.push(i);
-
-      } else if (
-
+    openStore.push(i);
 
   }
+}
+
+    if (openStore.length == closeStore.length) {
+
+      for (l = 0; l < closeStore.length; l++) {
+
+        if (closeStore[l] == l && closeStore[l] != 0)  {
+
+          totalClose++
+
+        } else if (openStore[l] == l && totalOpen != totalClose) {
+
+          totalOpen++
+
+        } else if (totalOpen == totalClose) {
+          itterations ++;
+          for (j = 0; j < totalClose; j++) {
+
+            if (parenth[j] == "(" && parenth[l] == ")") {
+
+              validPairs++
+
+            } else if (parenth[j] == "{" && parenth[l] == "}") {
+
+              validPairs++
+
+          } else if (parenth[j] == "[" && parenth[l] == "]") {
+
+            validPairs++
+
+        }
+
+      } 
+
+      if (validPairs == totalOpen) {
+
+        validRuns ++
+        totalClose = 0;
+        totalOpen = 0;
+
+
+      }
+      
+    }else {
+
+        console.log("invalid Parenth");
+
+      }
+
+    }
+
+    } else {
+
+      console.log("invalid parenth");
+
+    }
+
+    if (validRuns == itterations) {
+
+      console.log("EXTREME VALIDITY");
+
+    }
 
 }
+
 
 
 validParenth(testSubject);
